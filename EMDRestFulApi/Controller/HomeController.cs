47e7813e08c409afac92c78bb9d98a83e84209ef
unknown <thakurnilesh2000@gmail.com>
2025-08-06 17:23:24 +0530

    Initial commit Code.

---
+++ EMDRestFulApi/Controller/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EMDRestFulApi.Controller
+{
+    [Route("API/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> CompanyInfo()
+        {
+            return Ok();
+        }
+    }
+}
